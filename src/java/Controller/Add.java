369d8934abe54fad3ec499735afa900c2c65d905
chaudmhe141283 <chaudmhe141283@fpt.edu.vn>
2021-08-10 19:48:32 +0700

    test

--- src/java/Controller/Add.java
+++ src/java/Controller/Add.java
@@ -17,10 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Admin
- */
 @WebServlet(name = "Add", urlPatterns = {"/Add"})
 public class Add extends HttpServlet {
 
